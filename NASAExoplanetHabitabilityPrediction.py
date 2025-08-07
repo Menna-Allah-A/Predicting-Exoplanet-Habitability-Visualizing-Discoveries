dadf2d009a037582a8155b26f4731bc4fdcb8cf7
Menna <mennasabra3@gmail.com>
2025-08-07 23:16:22 +0300

    New

--- NASAExoplanetHabitabilityPrediction.py
+++ NASAExoplanetHabitabilityPrediction.py
@@ -1,27 +1,43 @@
-import requests  # ->allows Python to send HTTP requests (like GET/POST) to APIs or websites
-                 # -> to Fetch the data 
 import pandas as pd # -> Store the data you will get from AI response in a Structred DataFrame
 
 
-
-BaseUrl = "https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=exoplanets&select=pl_name,pl_orbper,pl_rade,pl_bmasse,st_teff,st_rad&format=json"
-
 try :
-
-    response = requests.get(BaseUrl) # -> Stores the response 
-    response.raise_for_status() # -> Rasies an error for bad status code 
-    data = response.json() # ->  Converts raw API data into a structure Python 
-    df = pd.DataFrame(data)  # -> Converts the JSON data into a Pandas DataFrame (a tabular structure)
-    
-    df.to_csv('nasa_exoplanets.csv' , index= False) # -> index = false for removing index column 
-
-    NewData = pd.read_csv('nasa_exoplanets')
-    print(NewData.head())
-except requests.exceptions.RequestException as e :
-    print("API Error:" , e)
+    df = pd.read_csv('PSCompPars.csv')
+    print(df.head())
 except pd.errors.EmptyDataError:
     print("NO received Data")
 except Exception as e:
     print("UnExpected error occured: ",e)        
 
 
+keyColumns = [ # Identification
+             'pl_name', 'hostname' ,
+                #  Planet characteristics
+              'pl_orbper', 'pl_orbsmax', 'pl_rade', 'pl_bmasse', 
+              'pl_bmassprov', 'pl_eqt', 'pl_controv_flag',
+              # Star characteristics
+              'st_spectype', 'st_teff', 'st_rad', 'st_mass', 'st_met',
+              # System characteristics
+              'sy_snum', 'sy_pnum', 'sy_dist',
+              # Discovery information
+              'discoverymethod', 'disc_year', 'disc_facility']
+
+df = df[keyColumns]
+
+
+
+
+
+
+
+
+
+
+# BaseUrl = "https://exoplanetarchive.ipac.caltech.edu/cgi-bin/TblView/nph-tblView?app=ExoTbls&config=PSCompPars"
+    #response = requests.get(BaseUrl) # -> Stores the response 
+    #response.raise_for_status() # -> Rasies an error for bad status code 
+    #data = response.json() # ->  Converts raw API data into a structure Python 
+    #df = pd.DataFrame(data)  # -> Converts the JSON data into a Pandas DataFrame (a tabular structure)
+    #df.to_csv('nasa_exoplanets.csv' , index= False) # -> index = false for removing index column 
+#import requests  # ->allows Python to send HTTP requests (like GET/POST) to APIs or websites
+                 # -> to Fetch the data 
